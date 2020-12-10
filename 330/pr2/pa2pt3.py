20766370aec27c666b5cc8105f0993d4ea0f682a
fgpacini <fgpacini@yahoo.com>
2020-12-10 12:15:07 -0500

    part 3

--- 330/pr2/pa2pt3.py
+++ 330/pr2/pa2pt3.py
@@ -12,7 +12,6 @@ import networkx as nx
 
 ############################
 G = nx.Graph()
-H = nx.Graph()
 pos = {}
 edge_colors = []
 node_colors = []
@@ -27,10 +26,8 @@ def readGraph(input_file, locations_file):
     node_colors = ["#1f78b4"]*N
     node_colors[s] = "#c0ff27"
     for edge in raw[3:]:
-        if(int(edge[0]) < int(edge[1])):
-            #edge_colors.append(["#ffffff", int(edge[0]), int(edge[1])])
-            G.add_edge(int(edge[0]), int(edge[1]))
         adj_list[int(edge[0])].append((int(edge[1]), edge[2]))
+
     with open(locations_file, 'r') as f:
         raw_locations = [[float(x) for x in s.split(',')] for s in f.read().splitlines()]
     for i, loc in enumerate(raw_locations[1:]):
@@ -88,36 +85,11 @@ def Run(input_file, locations_file, output_file):
     distances, parents =   dijkstra(N, m, s, adj_list)
     undirected_adj_list = make_undirected_graph(N, adj_list)
     mst = kruskal(N, m, undirected_adj_list)
-    findBetterTree(N, s, adj_list, distances, parents, mst)
-    writeOutput(output_file, N, s, distances, parents, mst)
-    return compareTrees(N, s, adj_list, distances, parents, mst)
-
-
-############################
-
-# ADD YOUR OWN METHODS HERE (IF YOU'D LIKE)
-
-############################
-
-############################
-
-# FINISH THESE METHODS
-
-############################
-
+    return findBetterTree(N, s, adj_list, distances, parents, mst)
 
 
 def dijkstra(N, m, s, adj_list):
-    # You are given the following variables:
-    # N = number of nodes in the graph
-    # m = number of edges in the graph
-    # s = source node for the algorithm
-    # adj_list = a list of lists of size N, where each index represents a node n
-    #               the sublist at index n has a list of two-tuples,
-    #               representing outgoing edges from n: (adjacent node, weight of edge)
-    #               NOTE: If a node has no outgoing edges, it is represented by an empty list
-    #
-    # WRITE YOUR CODE HERE:
+
     pi = [float('inf')]*N
     distances = [float('inf')]*N
     pi[s] = 0.0
@@ -141,20 +113,10 @@ def dijkstra(N, m, s, adj_list):
     # Return two lists of size N, in which each index represents a node n:
     # distances: the shortest distance from s to n
     # parents: the last (previous) node before n on the shortest path
-    #print(distances)
-    #print(parents)
     return distances, parents
 
 def kruskal(N, m, undirected_adj_list):
-    # You are given the following variables:
-    # N = number of nodes in the graph
-    # PLEASE NOTE THAT THE ADJACENCY LIST IS FORMATTED ENTIRELY DIFFERENTLY FROM DIJKSTRA ABOVE
-    # undireced_adj_list = a list of lists of size N, where each index represents a node n
-    #                       the sublist at index n has a list of two-tuples, representing edges from n: (adjacent node, weight of edge)
-    #                       NOTE: Since the graph is undirected, each edge (u,v) is now represented twice in this adjacency list:
-    #                               once at index u and once at index v
-    #
-    # WRITE YOUR CODE HERE:
+    
     E = []
     edgeDict = {}
     for i, adj in enumerate(undirected_adj_list):
@@ -188,73 +150,55 @@ def kruskal(N, m, undirected_adj_list):
     # Return the adjacency list for the MST, formatted as a list-of-lists in exactly the same way as undirected_adj_list
     return mst_adj_list
 
-def compareTrees(N, s, adj_list, sp_dist, sp_parents, mst):
-    C_sp = 0.0
-    for child, parent in enumerate(sp_parents):
-        if parent != None:
-            weight = 0
-            for e in adj_list[parent]:
-                if e[0] == child:
-                    weight = e[1]
-                    break
-            C_sp += weight
-            #edge_index = [(e[1], e[2]) for e in edge_colors].index((min(child, parent), max(child, parent)))
-            #edge_colors[edge_index][0] = "#fb2000"
-    
-    C_mst, mst_dist = getCostDist(N, s, mst)
-
-    TWR = C_sp / C_mst
-    # Create a list of all ratios (besides those where u = s or d_sp[u] = 0), and then sort to easily obtain MDR and other quantities
-    dist_ratios = [0 if sp_dist[u] == 0 or u == s else mst_dist[u]/sp_dist[u] for u in range(N)]
-    dist_ratios.sort()
-    MDR = dist_ratios[-1]
-    ADR = sum(dist_ratios) / sum([0 if sp_dist[i] == 0 else 1 for i in range(N)])
-    print(dist_ratios[-math.floor(N/100.0):]) # Look at largest 1% of nodes to see dropoff from the MDR
-    return TWR, MDR, ADR
-
 def findBetterTree(N, s, adj_list, sp_dist, sp_parents, mst):
     
-    mst_cost, mst_dist = getCostDist(N, s, mst)
+    mst_cost, mst_dist = getCostDist(N, s, mst) # Get cost and distance of the mst to use in the TWR and to get the highest DR nodes
+    """
+    for child, parent in enumerate(sp_parents):
+        if parent != None:
+            G.add_edge(child, parent)
+    """
 
     dist_ratios = [0 if sp_dist[u] == 0 or u == s else mst_dist[u]/sp_dist[u] for u in range(N)]
-    sorted_dist_ratios = sorted(list(enumerate(dist_ratios)), key = lambda x: x[1])
+    sorted_dist_ratios = sorted(list(enumerate(dist_ratios)), key = lambda x: x[1])     # Sort by the distance ratios, storing the node since it no longer corresponds to the index.
     a = 0
     b = N
-    while(a < b):
+    while(a < b):   
         i = -1
-        num = math.ceil((a + b)/2.0)
-        tree = [arr.copy() for arr in mst]
-        while abs(i) < num:
+        n = math.ceil((a + b)/2.0)  # Binary search for the minimizing value n
+        tree = [arr.copy() for arr in mst]      # Reset the tree each iteration back to the mst
+        while abs(i) < n:   # Start with the highest DR node, and add an edge only for the highest n
             u = sorted_dist_ratios[i][0]
             v = sp_parents[u]
-            if not (v == None or v in [e[0] for e in tree[u]]):
-                w = adj_list[u][[e[0] for e in adj_list[u]].index(v)][1]
+            if not (v == None or v in [e[0] for e in tree[u]]):     # If there is a valid edge not already in the tree
+                w = adj_list[u][[e[0] for e in adj_list[u]].index(v)][1]    # Weight not ctually needed but for consistency of tree adj list
                 tree[u].append((v, w))
                 tree[v].append((u, w))
             i -= 1
         
-        tree_dist, tree_parents = dijkstra(N, 0, s, tree)
+        tree_dist, tree_parents = dijkstra(N, 0, s, tree)   # Make it a tree by removing the now unnecessary (mst) edges
         tree_cost = 0.0
         for child, parent in enumerate(tree_parents):
             if parent != None:
                 weight = 0
                 for e in adj_list[parent]:
                     if e[0] == child:
-                        weight = e[1]
+                        weight = e[1]       # Calculate the cost for the new tree
                         break
                 tree_cost += weight
 
         TWR = tree_cost / mst_cost
         MDR = max([0 if sp_dist[u] == 0 or u == s else tree_dist[u]/sp_dist[u] for u in range(N)])
-        print(TWR, MDR, a, b, num)
-        if(TWR < MDR):
-            a = num + 1
+        
+        if(TWR < MDR):  # Can tighten considered range of values for n
+            a = n + 1
         else:
-            b = num - 1
+            b = n - 1
+
     for child, parent in enumerate(tree_parents):
         if parent != None:
-            H.add_edge(child, parent)
-    print(TWR, MDR, num)
+            G.add_edge(child, parent)   # Construct networkx graph of minimizing tree (for visualization)
+    print(TWR, MDR, n)
     return TWR, MDR
 
 
@@ -275,45 +219,17 @@ def getCostDist(N, s, undirected_adj_list):
                 q.put(neighbor)
                 cost += weight
                 dist[neighbor] = dist[node] + weight
+                #G.add_edge(child, parent)
 
     return cost, dist
 
-#############################
-# CHANGE INPUT FILES FOR PART 2 HERE
-
-#############################
-
 def main(args=[]):
-    # WHEN YOU SUBMIT TO THE AUTOGRADER, 
-    # PLEASE MAKE SURE THE FOLLOWING FUNCTION LOOKS LIKE:
-    # Run('input', 'output')
-    #Run('input', 'output')
-    #Run('g_randomEdges.txt', 'pa2_locations/g_randomLocations.txt', 'output')
+    Run('g_randomEdges.txt', 'pa2_locations/g_randomLocations.txt', 'output')
     #Run('g_donutEdges.txt', 'pa2_locations/g_donutLocations.txt', 'output')
-    Run('g_zigzagEdges.txt', 'pa2_locations/g_zigzagLocations.txt', 'output')
+    #Run('g_zigzagEdges.txt', 'pa2_locations/g_zigzagLocations.txt', 'output')
 
-    #global edge_colors
-    #edge_colors = [e[0] for e in edge_colors]
-    #nx.draw(G, node_size=10, pos=pos, edge_color="#ff1b1b", width=0.5)
-    nx.draw(H, node_size=10, pos=pos, edge_color="#210000", width=0.75)
+    nx.draw(G, node_size=10, pos=pos, edge_color="#210000", width=0.75)
     plt.show()
-    # AFTER YOUR RUN THE AUTOGRADER,
-    # you may change comment out the above line
-    # and uncomment the Run commend for the graph from part 2
-    # that you wish to work on:
-    
-    """
-    fig, ax = plt.subplots()
-    map = [('g_randomEdges.txt', "Random"), ('g_donutEdges.txt', "Donut"), ('g_zigzagEdges.txt', "Zigzag")]
-    for m in map:
-        x, y = Run(m[0], 'output')
-        ax.scatter(x, y, label=m[1])
-    ax.set_xlabel("Total Weight Ratio")
-    ax.set_ylabel("Maximum Distance Ratio")
-    ax.legend()
-    ax.set_title("Scatter Plot of Total Weight and Max Distance Ratios")
-    plt.show()
-    """
  
 if __name__ == "__main__":
     main(sys.argv[1:])    
