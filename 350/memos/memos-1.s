e09b314b37c67c8b75dd4806045c2dcc1fc43a9d
fgpacini <fgpacini@yahoo.com>
2021-02-26 15:08:48 -0500

    memos setup

--- 350/memos/memos-1.s
+++ 350/memos/memos-1.s
@@ -2,59 +2,17 @@
     .code16
 
 _start:
+	cli
+	hlt
     movw $0x9000, %ax
     movw %ax, %ss       # ss:sp == stack segment : offset
     xorw %sp, %sp
-
-
-    movw $0x0, %ax
-    movw %ax, %es
-    leaw msg, %di
-
-    xorl %ebx, %ebx
-    movl $0x534D4150, %edx
-    movl $0xE820, %eax
-    movl $0x18, %ecx
-    int $0x15
-    jc end
-
-    movw $0x0, %dx
-    movw %dx, %ds
-    leaw msg, %si
-
-1: 
-    lodsb               # Load a byte from DS:SI to AL & increment SI
-    movb $0x0E, %ah     # Write character to the screen
-    int $0x10           # Make BIOS interrupt call to vector 0x10
-    loop 1b             # Decrement CX, check for zero
-
-    jmp end
-
-print:	pushw %dx
-	movb %al, %dl
-	shrb $4, %al
-	cmpb $10, %al
-	jge 1f
-	addb $0x30, %al		# Add ASCII '0' offset
-	jmp 2f
-1:	addb $55, %al		# Add ASCII 'A'-10 offset
-2:	movb $0x0E, %ah
-	int $0x10                   # BIOS interrupt with AH=0x0E for teletype output of char in %ax with 
-                                # light gray color (%bx = 0x7)
-	movb %dl, %al
-	andb $0x0f, %al
-	cmpb $10, %al
-	jge 1f
-	addb $0x30, %al
-	jmp 2f
-1:	addb $55, %al		
-2:	movb $0x0E, %ah
+    movw $3, %ax
+    int $0x10
+	movb $65, %al
+	movb $0x0E, %ah
+	movw $0x07, %bx
 	int $0x10
-	popw %dx
-	ret
-
-msg: .asciz "                         "
-msg_len: .word . - msg
 
 end:
     hlt
@@ -67,7 +25,7 @@ end:
 	.byte 0xAA
 
 # To test:	
-# as --32 memos-1.s -o memos-1.o
-# ld -T memos.ld memos-1.o -o memos-1
-# dd bs=1 if=memos-1 of=memos-1_test skip=4096 count=512
-# qemu-system-i386 -hda memos-1_test | /root/vnc/opt/TigerVNC/bin/vncviewer localhost:5900  
+# as --32 vga16.s -o vga16.o
+# ld -T vga.ld vga16.o -o vga16
+# dd bs=1 if=vga16 of=vga16_test skip=4096 count=512
+# bochs -qf bochsrc-vga
